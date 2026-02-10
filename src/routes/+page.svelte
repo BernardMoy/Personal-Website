3caf6e89535851bcad8ea724e601346d36e4a00b
Bernard Moy <bernardmoy3@gmail.com>
2026-02-10 20:43:55 +0000

    Configured fonts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,15 @@
 	<Topbar />
 
 	<img src="/images/bg-1.png" alt="bg-1" class="h-screen w-full object-cover" />
+
+	<div class="font-main-title">Hi I'm Bernard</div>
+
+	<p class="font-body">
+		I graduated with a First Class Honours Bachelor's degree in Computer Science from the University
+		of Warwick in 2025, and is now studying for a Masters of Advanced Computing at Imperial College
+		London. I am interested in turning creative ideas into software and code, and enjoy constantly
+		learning new things. Currently looking for careers in full-stack software development.
+	</p>
 </main>
 
 <!-- reusable component
--- src/routes/Topbar.svelte
+++ src/routes/Topbar.svelte
@@ -19,8 +19,8 @@
 	];
 </script>
 
-<header class=" fixed top-0 flex w-full flex-row gap-16 bg-white/65 px-16 py-8">
+<header class="fixed top-0 flex w-full flex-row gap-16 bg-white/65 px-16 py-8">
 	{#each navigations as { name, navigationId }}
-		<a href={navigationId}>{name}</a>
+		<a href={navigationId} class="font-title">{name}</a>
 	{/each}
 </header>

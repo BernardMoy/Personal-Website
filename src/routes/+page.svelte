d29c175ad003fc73e5f1a3c1cc886caea3f6439a
Bernard Moy <bernardmoy3@gmail.com>
2026-02-10 18:44:11 +0000

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import Topbar from './Topbar.svelte';
+</script>
+
+<main>
+	<!-- the fixed position top navigation bar -->
+	<Topbar />
+
+	<img src="/images/bg-1.png" alt="bg-1" class="h-screen w-full object-cover" />
+</main>
+
+<!-- reusable component
+{#snippet formStep({ question, id, type }: { question: string; id: string; type: string })}
+	<article>
+		<div>
+			<label for={id}>{question}</label>
+			<input {type} {id} bind:value={formState[id]} />
+		</div>
+	</article>
+{/snippet} -->
---
+++ src/routes/Topbar.svelte
@@ -0,0 +1,26 @@
+<script>
+	const navigations = [
+		{
+			name: 'Top',
+			navigationId: '#top'
+		},
+		{
+			name: 'About Me',
+			navigationId: '#about-me'
+		},
+		{
+			name: 'Projects',
+			navigationId: '#projects'
+		},
+		{
+			name: 'Contact Me',
+			navigationId: '#contact-me'
+		}
+	];
+</script>
+
+<header class=" fixed top-0 flex w-full flex-row gap-16 bg-white/65 px-16 py-8">
+	{#each navigations as { name, navigationId }}
+		<a href={navigationId}>{name}</a>
+	{/each}
+</header>

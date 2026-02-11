0ac1076f3c9380b1f85236823d5f544c172b2b02
Bernard Moy <bernardmoy3@gmail.com>
2026-02-11 01:13:27 +0000

    Main layout for about me

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,7 +8,7 @@
 	<!-- the fixed position top navigation bar -->
 	<Topbar />
 
-	<!-- Main background -->
+	<!-- Top -->
 	<div class="relative text-center">
 		<img src="/images/bg-1.png" alt="bg-1" class=" h-screen w-full object-cover" />
 
@@ -34,12 +34,68 @@
 		</div>
 	</div>
 
-	<p class="font-body text-on-background">
-		I graduated with a First Class Honours Bachelor's degree in Computer Science from the University
-		of Warwick in 2025, and is now studying for a Masters of Advanced Computing at Imperial College
-		London. I am interested in turning creative ideas into software and code, and enjoy constantly
-		learning new things. Currently looking for careers in full-stack software development.
-	</p>
+	<!-- About me separator -->
+	<div class="relative z-10 h-[96px] w-full bg-primary"></div>
+
+	<!-- About me -->
+	<div class="relative z-0 h-screen w-full">
+		<!-- background image -->
+		<!-- this is relative to force the viewport height by the outer wrapper -->
+		<div class="absolute bottom-0 w-full">
+			<img src="/images/bg-2.png" alt="bg-2" class=" w-full object-cover opacity-45" />
+		</div>
+
+		<!-- header-->
+		<div class="relative">
+			<img
+				src="/svgs/header-grid.svg"
+				alt="header-grid"
+				class="h-[160px] w-full object-cover opacity-40"
+			/>
+			<p class="font-title absolute top-1/2 left-16 -translate-y-1/2 text-[48px] text-primary">
+				About Me
+			</p>
+			<img
+				src="/svgs/code-text.svg"
+				alt="code"
+				class="absolute top-1/2 right-0 h-[160px] -translate-y-1/2 object-cover"
+			/>
+		</div>
+
+		<!-- content -->
+		<div class="relative mx-16 my-24 flex flex-row items-start justify-center gap-16">
+			<!-- left paragraph -->
+			<div class="flex flex-3 flex-col items-start justify-start gap-8">
+				<p class="font-body text-on-background">
+					I graduated with a <span class="text-primary"
+						>First Class Honours Bachelor's degree in Computer Science</span
+					>
+					from the University of Warwick in 2025, and is now studying for a
+					<span class="text-primary">Masters of Advanced Computing</span> at Imperial College London.
+				</p>
+				<p class="font-body text-on-background">
+					I am interested in turning creative ideas into software and code, and enjoy constantly
+					learning new things. Currently looking for careers in full-stack software development.
+				</p>
+			</div>
+
+			<!-- right skills -->
+			<div
+				class="flex flex-1 flex-col items-start justify-start gap-8 rounded-[12px] bg-primary p-8"
+			>
+				<h1 class="font-title text-[36px] text-on-primary">Skills</h1>
+				<p class="font-body text-[18px] text-on-primary">
+					Python, Java, Kotlin, HTML, CSS, JavaScript, TypeScript, SQL, React, Git
+				</p>
+			</div>
+		</div>
+	</div>
+
+	<!-- Projects -->
+
+	<!-- Contact me -->
+
+	<!-- Ending -->
 
 	<!-- the fixed scroll to next page arrow down button -->
 	<div class="fixed bottom-8 flex w-full flex-col items-center">
--- src/routes/Topbar.svelte
+++ src/routes/Topbar.svelte
@@ -21,7 +21,7 @@
 
 <!-- currently it has a fixed height of 80px -->
 <header
-	class="fixed top-0 z-10 flex h-[80px] w-full flex-row items-center gap-16 bg-white/65 px-16"
+	class="fixed top-0 z-50 flex h-[80px] w-full flex-row items-center gap-16 bg-white/65 px-16"
 >
 	{#each navigations as { name, navigationId }}
 		<a href={navigationId} class="font-title">{name}</a>

7617292b4d3b29e6eb3687423690a3003d9679f6
Bernard Moy <bernardmoy3@gmail.com>
2026-02-12 16:31:37 +0000

    About me page now uses the same layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,82 +44,67 @@
 	/>
 
 	<!-- About me -->
-	<div class="relative z-0 min-h-screen w-full">
+	<section class="relative min-h-screen w-full">
 		<!-- background image -->
-		<!-- this is relative to force the viewport height by the outer wrapper -->
-		<div class="absolute bottom-0 w-full">
-			<img src="/images/bg-2.png" alt="bg-2" class=" min-h-screen w-full object-cover opacity-45" />
+		<div class="sticky top-0 h-screen w-full">
+			<img src="/images/bg-2.png" alt="bg-2" class="h-full w-full object-cover opacity-40" />
 		</div>
-
 		<!-- additional deco -->
-		<img
-			src="/svgs/deco-circle-dot.svg"
-			alt="decoration"
-			class=" absolute top-12 -left-55 scale-75 rotate-90"
-		/>
-		<img
-			src="/svgs/deco-circle-dot.svg"
-			alt="decoration"
-			class="absolute -right-48 bottom-4 rotate-260"
-		/>
-
-		<!-- header-->
-		<div class="relative">
-			<!-- header image -->
-			<img
-				src="/svgs/header-grid.svg"
-				alt="header-grid"
-				class="h-[160px] w-full object-cover opacity-40"
-			/>
-			<!-- header title -->
-			<p
-				class="font-title no-selection absolute top-1/2 left-16 -translate-y-1/2 text-[48px] text-holo-blue"
-			>
-				About Me
-			</p>
-			<!-- additional deco -->
-			<img
-				src="/svgs/code-text.svg"
-				alt="code"
-				class="absolute top-1/2 right-0 h-[160px] -translate-y-1/2 object-cover"
-			/>
-		</div>
 
 		<!-- content -->
-		<div class="relative mx-16 my-18 flex flex-row items-start justify-center gap-16">
-			<!-- left paragraph -->
-			<div class="flex flex-3 flex-col items-start justify-start gap-8">
-				<p class="font-body text-[18px] text-on-background">
-					I graduated with a <span class="text-holo-blue"
-						>First Class Honours Bachelor's degree in Computer Science</span
-					>
-					from the University of Warwick in 2025, and is now studying for a
-					<span class="text-holo-blue">Masters of Advanced Computing</span> at Imperial College London.
-				</p>
-				<p class="font-body text-[18px] text-on-background">
-					I am interested in turning creative ideas into software and code, and enjoy constantly
-					learning new things. Currently looking for careers in full-stack software development.
-				</p>
+		<div class="relative -mt-[100vh] flex w-full flex-col gap-16">
+			<!-- header image -->
+			<div class="relative">
+				<img
+					src="/svgs/header-grid.svg"
+					alt="header-grid"
+					class="h-[160px] w-full object-cover opacity-40"
+				/>
+				<!-- header title -->
+				<h1
+					class="font-title no-selection absolute top-1/2 left-16 -translate-y-1/2 text-[48px] text-holo-blue"
+				>
+					About Me
+				</h1>
 			</div>
 
-			<!-- right skills -->
-			<div class="flex flex-1 flex-col items-start justify-start gap-8 rounded-xl bg-holo-blue p-8">
-				<!-- skills title -->
-				<h1 class="font-title holo-blue-selection no-selection text-[36px] text-on-primary">
-					Skills
-				</h1>
+			<div class="relative mx-16 my-18 flex flex-row items-start justify-center gap-16">
+				<!-- left paragraph -->
+				<div class="flex flex-3 flex-col items-start justify-start gap-8">
+					<p class="font-body text-[18px] text-on-background">
+						I graduated with a <span class="text-holo-blue"
+							>First Class Honours Bachelor's degree in Computer Science</span
+						>
+						from the University of Warwick in 2025, and is now studying for a
+						<span class="text-holo-blue">Masters of Advanced Computing</span> at Imperial College London.
+					</p>
+					<p class="font-body text-[18px] text-on-background">
+						I am interested in turning creative ideas into software and code, and enjoy constantly
+						learning new things. Currently looking for careers in full-stack software development.
+					</p>
+				</div>
 
-				<!-- skills list -->
-				<ul class="grid w-full grid-cols-2 gap-x-4">
-					{#each skills as skill}
-						<li class="font-body holo-blue-selection text-[18px] text-on-primary">
-							<span class="no-selection">⋄&nbsp</span>{skill}
-						</li>
-					{/each}
-				</ul>
+				<!-- right skills -->
+				<div
+					class="flex flex-1 flex-col items-start justify-start gap-8 rounded-xl bg-holo-blue p-8"
+				>
+					<!-- skills title -->
+					<h1 class="font-title holo-blue-selection no-selection text-[36px] text-on-primary">
+						Skills
+					</h1>
+
+					<!-- skills list -->
+					<ul class="grid w-full grid-cols-2 gap-x-4">
+						{#each skills as skill}
+							<li class="font-body holo-blue-selection text-[18px] text-on-primary">
+								<span class="no-selection">⋄&nbsp</span>{skill}
+							</li>
+						{/each}
+					</ul>
+				</div>
 			</div>
 		</div>
-	</div>
+	</section>
 
 	<!-- Projects separator -->
 	<img
@@ -137,7 +122,7 @@
 		<!-- additional deco -->
 
 		<!-- content -->
-		<div class="relative -mt-[100vh] flex w-full flex-col gap-16">
+		<div class="relative -mt-[100vh] flex w-full flex-col">
 			<!-- header image -->
 			<div class="relative">
 				<img
@@ -154,51 +139,53 @@
 			</div>
 
 			<!-- intro paragraph -->
-			<p class="font-body mx-16 text-[18px] text-on-background">
-				Here is a list of technical projects that I have done.
-			</p>
+			<div class="mx-16 flex flex-col gap-16 py-16">
+				<p class="font-body text-[18px] text-on-background">
+					Here is a list of technical projects that I have done.
+				</p>
 
-			<!-- project list -->
-			<div class="mx-16 grid grid-cols-3 gap-8 pb-16">
-				<!-- individual project grid -->
-				{#each projects as project}
-					<div class="relative flex w-full flex-col rounded-xl bg-on-primary pb-6">
-						<!-- image -->
-						<img
-							src={project.image}
-							alt={project.title}
-							class="h-[220px] w-full rounded-tl-xl rounded-tr-xl object-cover"
-						/>
+				<!-- project list -->
+				<div class=" grid grid-cols-3 gap-8">
+					<!-- individual project grid -->
+					{#each projects as project}
+						<div class="relative flex w-full flex-col rounded-xl bg-on-primary pb-6">
+							<!-- image -->
+							<img
+								src={project.image}
+								alt={project.title}
+								class="h-[220px] w-full rounded-tl-xl rounded-tr-xl object-cover"
+							/>
 
-						<!-- github logo -->
-						{#if project.github}
-							<a
-								href={project.github}
-								target="_blank"
-								class="cursor-pointer"
-								aria-label="github link"
-							>
-								<img
-									src="svgs/github.svg"
-									alt="github logo"
-									class="absolute top-2 right-2 h-[64px] w-[64px]"
-								/>
-							</a>
-						{/if}
+							<!-- github logo -->
+							{#if project.github}
+								<a
+									href={project.github}
+									target="_blank"
+									class="cursor-pointer"
+									aria-label="github link"
+								>
+									<img
+										src="svgs/github.svg"
+										alt="github logo"
+										class="absolute top-2 right-2 h-[64px] w-[64px]"
+									/>
+								</a>
+							{/if}
 
-						<!-- title -->
-						<h1 class="font-title relative px-6 py-4 text-[24px] text-on-background">
-							{project.title}
-						</h1>
+							<!-- title -->
+							<h1 class="font-title relative px-6 py-4 text-[24px] text-on-background">
+								{project.title}
+							</h1>
 
-						<!-- tags -->
-						<ul class="flex max-w-full flex-wrap gap-x-4 gap-y-2 px-6">
-							{#each project.tags as tag}
-								<li class="font-body bg-amber-200 px-2 text-[18px] text-on-background">{tag}</li>
-							{/each}
-						</ul>
-					</div>
-				{/each}
+							<!-- tags -->
+							<ul class="flex max-w-full flex-wrap gap-x-4 gap-y-2 px-6">
+								{#each project.tags as tag}
+									<li class="font-body bg-amber-200 px-2 text-[18px] text-on-background">{tag}</li>
+								{/each}
+							</ul>
+						</div>
+					{/each}
+				</div>
 			</div>
 		</div>
 	</section>

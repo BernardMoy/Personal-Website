4a33ea60ebd8047aaf7cc9e00aedcc93990c7576
Bernard Moy <bernardmoy3@gmail.com>
2026-02-16 00:19:08 +0000

    Used inline SVG

--- src/routes/CircleArrow.svelte
+++ src/routes/CircleArrow.svelte
@@ -7,5 +7,12 @@
 	aria-label="next page"
 	{onclick}
 >
-	<img src="/svgs/arrow-down.svg" alt="arrow down" />
+	<svg width="28" height="15" fill="none" viewBox="0 0 28 15"
+		><path
+			fill="#fff"
+			fill-rule="evenodd"
+			d="M28 1.896 26.032 0l-12.05 11.265-1.285-1.202.007.007L1.998.061 0 1.928C2.958 4.695 11.22 12.418 13.982 15c2.052-1.917.052-.047 14.018-13.104"
+			clip-rule="evenodd"
+		/></svg
+	>
 </button>

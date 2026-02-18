823b18d285e01f7df40fbefdfabf8748331e69ae
Bernard Moy <bernardmoy3@gmail.com>
2026-02-18 13:14:48 +0000

    Able to switch images using numbers or right images

--- src/routes/Top.svelte
+++ src/routes/Top.svelte
@@ -1,29 +1,18 @@
 <script>
 	import Circle from './Circle.svelte';
 	import top from '../data/top.json';
+	import backgrounds from '../data/backgrounds.json';
 
-	const backgrounds = [
-		{
-			name: 'bg-1',
-			url: '/images/bg-1-1.png'
-		},
-		{
-			name: 'bg-2',
-			url: '/images/bg-1-2.png'
-		},
-		{
-			name: 'bg-3',
-			url: '/images/bg-1-3.png'
-		},
-		{
-			name: 'bg-4',
-			url: '/images/bg-1-4.png'
-		}
-	];
+	// the current index of the image displayed
+	var currentIndex = $state(0);
 </script>
 
 <div class="relative text-center">
-	<img src="/images/bg-1-1.png" alt="bg-1" class=" h-screen w-full object-cover" />
+	<img
+		src={backgrounds[currentIndex].url}
+		alt={backgrounds[currentIndex].name}
+		class="h-screen w-full object-cover"
+	/>
 
 	<!-- centered text -->
 	<div class="absolute top-1/2 flex w-full -translate-y-1/2 flex-col items-center">
@@ -31,21 +20,27 @@
 		<h2 class="font-main-title text-[36px] text-on-primary">{top.subtitle}</h2>
 	</div>
 
-	<!-- 1 2 3 4 buttons -->
+	<!-- number buttons on the left -->
 	<div class="no-selection absolute top-0 m-4 mt-[96px] flex flex-col gap-4">
-		{#each { length: 4 } as _, i}
-			<Circle text={(i + 1).toString()} onclick={() => {}}></Circle>
+		{#each { length: backgrounds.length } as _, i}
+			<Circle
+				text={(i + 1).toString()}
+				onclick={() => {
+					currentIndex = i;
+				}}
+			></Circle>
 		{/each}
 	</div>
 
-	<!-- the 4 rectangles on the top right -->
+	<!-- the rectangles on the top right displayed in 2 columns -->
 	<div class="absolute top-0 right-0 mt-[80px] flex flex-col">
 		<div class="no-selection grid grid-cols-2">
-			{#each backgrounds as bg}
+			{#each backgrounds as bg, index}
 				<button
 					aria-label={bg.name}
 					class="h-[64px] w-[160px] cursor-pointer bg-cover bg-center"
 					style={`background-image: url(${bg.url})`}
+					onclick={() => (currentIndex = index)}
 				></button>
 			{/each}
 		</div>
